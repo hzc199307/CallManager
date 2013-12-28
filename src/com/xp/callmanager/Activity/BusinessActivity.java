7dca9fffea0980ae9fd5955ea6f794763ba4e2da
hzc199307 <hzc199307@qq.com>
2013-12-28 13:53:16 +0800

    Signed-off-by: hzc199307 <hzc199307@qq.com>

---
+++ src/com/xp/callmanager/Activity/BusinessActivity.java
@@ -0,0 +1,26 @@
+package com.xp.callmanager.Activity;
+
+import com.xp.callmanager.R;
+import com.xp.callmanager.R.layout;
+import com.xp.callmanager.R.menu;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class BusinessActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_business);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.business, menu);
+		return true;
+	}
+
+}
