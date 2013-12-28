3310b772c7ad5a91eab853d13d294dcd4c46ce0a
WPearl <316018346@qq.com>
2013-12-28 22:34:59 +0800

    Signed-off-by: WPearl <316018346@qq.com>

--- src/com/xp/callmanager/Activity/BusinessActivity.java
+++ src/com/xp/callmanager/Activity/BusinessActivity.java
@@ -3,8 +3,10 @@ package com.xp.callmanager.Activity;
 import java.util.ArrayList;
 
 import com.xp.callmanager.R;
+import com.xp.callmanager.more.Activity.AttributionActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.view.Menu;
@@ -12,6 +14,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 /**
@@ -21,41 +24,31 @@ import android.widget.Toast;
  * 
  */
 public class BusinessActivity extends Activity {
-	private EditText numberPhone;
-	private EditText content;
-	private Button but_send;
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_business);
-		numberPhone = (EditText) findViewById(R.id.numberPhone);
-		content = (EditText) findViewById(R.id.content);
-		but_send = (Button) findViewById(R.id.send);
-		but_send.setOnClickListener(new sendOnclickListener());
-	}
+		RelativeLayout attributionButton = (RelativeLayout)findViewById(R.id.attributionButton);
+		attributionButton.setOnClickListener(new RelativeLayout.OnClickListener() {
 
-	private final class sendOnclickListener implements OnClickListener {
-		@Override
-		public void onClick(View v) {
-			String numberPhone2 = numberPhone.getText().toString();
-			String content2 = content.getText().toString();
-			SmsManager sms = SmsManager.getDefault();
-			ArrayList<String> texts = sms.divideMessage(content2);// 拆分短信
-			for (String text : texts) {
-				sms.sendTextMessage(numberPhone2, null, text, null, null);
+			@Override
+			public void onClick(View v) {
+				// TODO 自动生成的方法存根
+				Intent intent = new Intent(BusinessActivity.this,
+						SenterActivity.class); // 跳转到号码归属地查询页面
+				startActivity(intent);
+				BusinessActivity.this.finish();
 			}
-			Toast.makeText(getApplicationContext(), "发送成功", Toast.LENGTH_SHORT)
-					.show();
-		}
-
+		});
+		;
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.business, menu);
+		getMenuInflater().inflate(R.menu.more, menu);
 		return true;
 	}
+
 
 }
