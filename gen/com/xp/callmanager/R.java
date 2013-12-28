edbbfd6c28dab29c992a2cdb00075c7e8ede2e30
WPearl <316018346@qq.com>
2013-12-28 22:34:59 +0800

    Signed-off-by: hzc199307 <hzc199307@qq.com>

--- gen/com/xp/callmanager/R.java
+++ gen/com/xp/callmanager/R.java
@@ -39,21 +39,23 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f080006;
-        public static final int add_friend=0x7f080005;
-        public static final int attributionButton=0x7f080004;
-        public static final int content=0x7f080001;
-        public static final int hello=0x7f080003;
-        public static final int numberPhone=0x7f080000;
-        public static final int send=0x7f080002;
+        public static final int add_friend=0x7f080001;
+        public static final int attributionButton=0x7f080000;
+        public static final int content=0x7f080004;
+        public static final int hello=0x7f080002;
+        public static final int numberPhone=0x7f080003;
+        public static final int send=0x7f080005;
     }
     public static final class layout {
         public static final int activity_attribution=0x7f030000;
         public static final int activity_business=0x7f030001;
         public static final int activity_main=0x7f030002;
         public static final int activity_more=0x7f030003;
-        public static final int activity_tabs=0x7f030004;
-        public static final int activity_tongxun=0x7f030005;
-        public static final int call_manager=0x7f030006;
+        public static final int activity_senter=0x7f030004;
+        public static final int activity_senter1=0x7f030005;
+        public static final int activity_tabs=0x7f030006;
+        public static final int activity_tongxun=0x7f030007;
+        public static final int call_manager=0x7f030008;
     }
     public static final class menu {
         public static final int attribution=0x7f070000;
@@ -61,8 +63,9 @@ public final class R {
         public static final int call_manager=0x7f070002;
         public static final int main=0x7f070003;
         public static final int more=0x7f070004;
-        public static final int tabs=0x7f070005;
-        public static final int tongxun=0x7f070006;
+        public static final int senter=0x7f070005;
+        public static final int tabs=0x7f070006;
+        public static final int tongxun=0x7f070007;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -72,6 +75,7 @@ public final class R {
         public static final int title_activity_business=0x7f050006;
         public static final int title_activity_call_manager=0x7f050003;
         public static final int title_activity_more=0x7f050007;
+        public static final int title_activity_senter=0x7f050009;
         public static final int title_activity_tabs=0x7f050004;
         public static final int title_activity_tongxun=0x7f050005;
     }
