920db3efd733bb0cae1eb075d5053f1c1269eeb6
WPearl <316018346@qq.com>
2013-12-28 15:26:16 +0800

    Signed-off-by: WPearl <316018346@qq.com>

--- src/com/xp/callmanager/Activity/BusinessActivity.java
+++ src/com/xp/callmanager/Activity/BusinessActivity.java
@@ -19,6 +19,7 @@ public class BusinessActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_business);
+		;
 	}
 
 	@Override
